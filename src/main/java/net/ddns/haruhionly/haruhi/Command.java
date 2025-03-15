30539d215e1cf77776a31f27b2bc79ab6a35495c
Noah Wellman <nwellman@arizona.edu>
2025-03-15 01:13:40 -0700

    Road map and blank command handler

---
+++ src/main/java/net/ddns/haruhionly/haruhi/Command.java
@@ -0,0 +1,16 @@
+package net.ddns.haruhionly.haruhi;
+
+import java.util.logging.Logger;
+
+import org.bukkit.command.TabExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.command.Command;
+
+
+public class HaruhiCommand implements TabExecutor {
+	public boolean onCommand (CommandSender sender, Command command,
+		String label, String[] args) {
+		return false;
+	}
+}
+
